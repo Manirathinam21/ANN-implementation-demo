b13380064e0f9147d4bb761603d82fedd38b4992
Manirathinam21 <manirathinam21@gmail.com>
2021-10-10 22:04:44 +0530

    updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="src",
     version="0.0.1",
-    author="c17hawke",
+    author="Manirathinam",
     description="A small package for ANN Implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
